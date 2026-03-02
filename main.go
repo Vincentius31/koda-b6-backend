231815f3616291cb412cc272c83366b904c279ea
Vincentius <vincentius85@gmail.com>
2026-03-02 14:00:31 +0700

    feat: adding corsMiddleware for global middleware

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
-	"os"
 	"koda-b6-backend/routes"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
+func corsMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "http://localhost:8888")
+		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.Data(http.StatusOK, "", []byte(""))
+		} else {
+			ctx.Next()
+		}
+
+	}
+}
+
 func main() {
+	r := gin.Default()
+	r.Use(corsMiddleware())
+
 	if _, err := os.Stat("uploads"); os.IsNotExist(err) {
 		os.Mkdir("uploads", 0755)
 	}
 
-	r := gin.Default()
 	r.Static("/uploads", "./uploads")
-	
+
 	routes.SetupRoutes(r)
 
 	r.Run("localhost:8888")
