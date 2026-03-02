3646ca5e2084e919790669512bd07cc767f210e2
Vincentius <vincentius85@gmail.com>
2026-03-02 13:10:04 +0700

    feat: adding the previous working from backend1

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"os"
+	"koda-b6-backend/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	if _, err := os.Stat("uploads"); os.IsNotExist(err) {
+		os.Mkdir("uploads", 0755)
+	}
+
+	r := gin.Default()
+	r.Static("/uploads", "./uploads")
+	
+	routes.SetupRoutes(r)
+
+	r.Run("localhost:8888")
+}
