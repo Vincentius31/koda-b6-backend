dd7160ab9b9a831b547e76db99c06730de66b136
Vincentius <vincentius85@gmail.com>
2026-03-02 15:26:33 +0700

    fix: fixing the routes that before running local and now using the db connection using pgx

--- routes/routes.go
+++ routes/routes.go
@@ -3,14 +3,35 @@ package routes
 import (
 	"koda-b6-backend/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
-func SetupRoutes(r *gin.Engine) {
-	// Routes Users
-	r.POST("/register", handlers.RegisterHandler)
-	r.POST("/login", handlers.LoginHandler)
-	r.GET("/users", handlers.GetAllUsersHandler)
-	r.GET("/users/:id", handlers.GetUserByIdHandler)
-	r.PATCH("/users/:id", handlers.UpdateUserHandler)
-	r.DELETE("/users/:id", handlers.DeleteUserHandler)
+// SetupRoutes menerima parameter conn agar bisa digunakan oleh handler
+func SetupRoutes(r *gin.Engine, conn *pgx.Conn) {
+	
+	// --- User Routes ---
+	
+	r.POST("/register", func(ctx *gin.Context) {
+		handlers.RegisterHandler(ctx, conn)
+	})
+
+	r.POST("/login", func(ctx *gin.Context) {
+		handlers.LoginHandler(ctx, conn)
+	})
+
+	r.GET("/users", func(ctx *gin.Context) {
+		handlers.GetAllUsersHandler(ctx, conn)
+	})
+
+	r.GET("/users/:id", func(ctx *gin.Context) {
+		handlers.GetUserByIdHandler(ctx, conn)
+	})
+
+	r.PATCH("/users/:id", func(ctx *gin.Context) {
+		handlers.UpdateUserHandler(ctx, conn)
+	})
+
+	r.DELETE("/users/:id", func(ctx *gin.Context) {
+		handlers.DeleteUserHandler(ctx, conn)
+	})
 }
