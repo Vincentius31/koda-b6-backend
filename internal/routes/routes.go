7bbfced92d294e2b3959d8f89288c19c05093705
Vincentius <vincentius85@gmail.com>
2026-03-05 10:19:05 +0700

    feat: adding routes that separated between user and public

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"koda-b6-backend/internal/di"
+	"koda-b6-backend/internal/middleware"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 )
@@ -11,12 +13,14 @@ func SetupRoutes(r *gin.Engine, conn *pgx.Conn) {
 	
 	userHandler := container.UserHandler()
 
+	r.POST("/login", userHandler.Login)
+	r.POST("/register", userHandler.Create)
+
 	userRoutes := r.Group("/users")
+	userRoutes.Use(middleware.AuthMiddleware())
 	{
 		userRoutes.GET("", userHandler.GetAll)
 		userRoutes.GET("/:id", userHandler.GetByID)
-		userRoutes.POST("", userHandler.Create)
-		userRoutes.POST("/login", userHandler.Login)
 		userRoutes.PATCH("/:id/upload", userHandler.UploadProfile)
 		userRoutes.PUT("/:id", userHandler.Update)
 		userRoutes.DELETE("/:id", userHandler.Delete)
