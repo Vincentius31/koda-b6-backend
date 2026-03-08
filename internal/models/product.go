ab0e89dbe1019eb4cb3fc87057b637fd931e445c
Vincentius <vincentius85@gmail.com>
2026-03-08 23:27:11 +0700

    feat: adding models for the products CRUD

---
+++ internal/models/product.go
@@ -0,0 +1,18 @@
+package models
+
+type Product struct {
+	IDProduct int    `json:"id_product"`
+	Name      string `json:"name"`
+	Desc      string `json:"desc"`
+	Price     int    `json:"price"`
+	Quantity  int    `json:"quantity"`
+	IsActive  bool   `json:"is_active"`
+}
+
+type CreateProductRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Desc     string `json:"desc"`
+	Price    int    `json:"price" binding:"required,min=1"`
+	Quantity int    `json:"quantity" binding:"min=0"`
+	IsActive bool   `json:"is_active"`
+}
