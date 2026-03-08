6985b73864d5d6458b79d1c9ad1d692160575f47
Vincentius <vincentius85@gmail.com>
2026-03-08 21:33:24 +0700

    adding getbyid, update and delete for the roles

--- internal/repository/roles.go
+++ internal/repository/roles.go
@@ -42,3 +42,25 @@ func (r *RoleRepository) FindAll(ctx context.Context)([]models.Role, error){
 	}
 	return roles, nil
 }
+
+func (r *RoleRepository) FindByID(ctx context.Context, id int) (*models.Role, error) {
+	query := `SELECT id_roles, name_roles FROM roles WHERE id_roles = $1`
+	var role models.Role
+	err := r.db.QueryRow(ctx, query, id).Scan(&role.IDRoles, &role.NameRoles)
+	if err != nil {
+		return nil, err
+	}
+	return &role, nil
+}
+
+func (r *RoleRepository) Update(ctx context.Context, id int, role models.Role) error {
+	query := `UPDATE roles SET name_roles = $1 WHERE id_roles = $2`
+	_, err := r.db.Exec(ctx, query, role.NameRoles, id)
+	return err
+}
+
+func (r *RoleRepository) Delete(ctx context.Context, id int) error {
+	query := `DELETE FROM roles WHERE id_roles = $1`
+	_, err := r.db.Exec(ctx, query, id)
+	return err
+}
