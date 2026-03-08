bf55f265a3b7d43b6bdbb1f0e390d1cf94465f7d
Vincentius <vincentius85@gmail.com>
2026-03-08 22:54:27 +0700

    feat: adding handlers for the category CRUD

---
+++ internal/handlers/category.go
@@ -0,0 +1,108 @@
+package handlers
+
+import (
+	"koda-b6-backend/internal/models"
+	"koda-b6-backend/internal/service"
+	"net/http"
+	"strconv"
+	"github.com/gin-gonic/gin"
+)
+
+type CategoryHandler struct {
+	service *service.CategoryService
+}
+
+func NewCategoryHandler(s *service.CategoryService) *CategoryHandler {
+	return &CategoryHandler{service: s}
+}
+
+func (h *CategoryHandler) Create(ctx *gin.Context) {
+	var req models.CreateCategoryRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, models.WebResponse{
+			Success: false, 
+			Message: err.Error(),
+		})
+		return
+	}
+	if err := h.service.Create(ctx.Request.Context(), req); err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.WebResponse{
+			Success: false, 
+			Message: err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusCreated, models.WebResponse{
+		Success: true, 
+		Message: "Category created",
+	})
+}
+
+func (h *CategoryHandler) GetAll(ctx *gin.Context) {
+	data, err := h.service.GetAll(ctx.Request.Context())
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.WebResponse{
+			Success: false, 
+			Message: err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, models.WebResponse{
+		Success: true, 
+		Data: data,
+	})
+}
+
+func (h *CategoryHandler) GetByID(ctx *gin.Context) {
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	data, err := h.service.GetByID(ctx.Request.Context(), id)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, models.WebResponse{
+			Success: false, 
+			Message: "Category not found",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, models.WebResponse{
+		Success: true, 
+		Data: data,
+	})
+}
+
+func (h *CategoryHandler) Update(ctx *gin.Context) {
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	var req models.CreateCategoryRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, models.WebResponse{
+			Success: false, 
+			Message: err.Error(),
+		})
+		return
+	}
+	if err := h.service.Update(ctx.Request.Context(), id, req); err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.WebResponse{
+			Success: false, 
+			Message: err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, models.WebResponse{
+		Success: true, 
+		Message: "Category updated",
+	})
+}
+
+func (h *CategoryHandler) Delete(ctx *gin.Context) {
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	if err := h.service.Delete(ctx.Request.Context(), id); err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.WebResponse{
+			Success: false, 
+			Message: err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, models.WebResponse{
+		Success: true, 
+		Message: "Category deleted",
+	})
+}
