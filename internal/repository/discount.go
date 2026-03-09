b918a651548fd2f5647f9c4ac2635ed021d52f95
Vincentius <vincentius85@gmail.com>
2026-03-09 10:35:18 +0700

    feat: adding repository for discount CRUD to database

---
+++ internal/repository/discount.go
@@ -0,0 +1,60 @@
+package repository
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"koda-b6-backend/internal/models"
+)
+
+type DiscountRepository struct {
+	db *pgx.Conn
+}
+
+func NewDiscountRepository(db *pgx.Conn) *DiscountRepository {
+	return &DiscountRepository{db: db}
+}
+
+func (r *DiscountRepository) Create(ctx context.Context, d models.Discount) error {
+	query := `INSERT INTO discount (product_id, discount_rate, description, is_flash_sale) VALUES ($1, $2, $3, $4)`
+	_, err := r.db.Exec(ctx, query, d.ProductID, d.DiscountRate, d.Description, d.IsFlashSale)
+	return err
+}
+
+func (r *DiscountRepository) FindAll(ctx context.Context) ([]models.Discount, error) {
+	query := `SELECT id_discount, product_id, discount_rate, description, is_flash_sale FROM discount`
+	rows, err := r.db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var discounts []models.Discount
+	for rows.Next() {
+		var d models.Discount
+		rows.Scan(&d.IDDiscount, &d.ProductID, &d.DiscountRate, &d.Description, &d.IsFlashSale)
+		discounts = append(discounts, d)
+	}
+	return discounts, nil
+}
+
+func (r *DiscountRepository) FindByID(ctx context.Context, id int) (*models.Discount, error) {
+	var d models.Discount
+	query := `SELECT id_discount, product_id, discount_rate, description, is_flash_sale FROM discount WHERE id_discount = $1`
+	err := r.db.QueryRow(ctx, query, id).Scan(&d.IDDiscount, &d.ProductID, &d.DiscountRate, &d.Description, &d.IsFlashSale)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+func (r *DiscountRepository) Update(ctx context.Context, id int, d models.Discount) error {
+	query := `UPDATE discount SET product_id=$1, discount_rate=$2, description=$3, is_flash_sale=$4 WHERE id_discount=$5`
+	_, err := r.db.Exec(ctx, query, d.ProductID, d.DiscountRate, d.Description, d.IsFlashSale, id)
+	return err
+}
+
+func (r *DiscountRepository) Delete(ctx context.Context, id int) error {
+	query := `DELETE FROM discount WHERE id_discount = $1`
+	_, err := r.db.Exec(ctx, query, id)
+	return err
+}
