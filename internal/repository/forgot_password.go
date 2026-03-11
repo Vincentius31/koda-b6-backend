5d8dd2f620fa9a373a7044ce49bd0e6990350feb
Vincentius <vincentius85@gmail.com>
2026-03-11 10:18:21 +0700

    fix: fixing the name of the method

--- internal/repository/forgot_password.go
+++ internal/repository/forgot_password.go
@@ -17,13 +17,13 @@ func NewForgotPasswordRepository(db *pgx.Conn) *ForgotPasswordRepository {
 	}
 }
 
-func (r *ForgotPasswordRepository) CreateData(ctx context.Context, email string, code int) error {
+func (r *ForgotPasswordRepository) CreateForgetRequest(ctx context.Context, email string, code int) error {
 	query := `INSERT INTO forgot_password (email, otp_code) VALUES ($1, $2)`
 	_, err := r.db.Exec(ctx, query, email, code)
 	return err
 }
 
-func (r *ForgotPasswordRepository) GetDataByEmailCode(ctx context.Context, email string, code int) (*models.ForgotPassword, error) {
+func (r *ForgotPasswordRepository) GetDataByEmailAndCode(ctx context.Context, email string, code int) (*models.ForgotPassword, error) {
 	query := `SELECT id_request, email, otp_code, created_at 
               FROM forgot_password 
               WHERE email = $1 AND otp_code = $2`
@@ -42,7 +42,7 @@ func (r *ForgotPasswordRepository) GetDataByEmailCode(ctx context.Context, email
 	return &data, nil
 }
 
-func (r *ForgotPasswordRepository) DeleteDataByCode(ctx context.Context, code int) error {
+func (r *ForgotPasswordRepository) DeleteByCode(ctx context.Context, code int) error {
 	query := `DELETE FROM forgot_password WHERE otp_code = $1`
 	_, err := r.db.Exec(ctx, query, code)
 	return err
