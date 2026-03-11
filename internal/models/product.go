165b665c04aa56bfcf3fb82c273365fc2c2fe2d7
Vincentius <vincentius85@gmail.com>
2026-03-12 02:49:30 +0700

    feat: adding new DTO for the recommended product

--- internal/models/product.go
+++ internal/models/product.go
@@ -24,3 +24,12 @@ type UpdateProductRequest struct {
 	Quantity *int    `json:"quantity"`
 	IsActive *bool   `json:"is_active"`
 }
+
+type ProductLanding struct {
+    IDProduct   int    `db:"id_product" json:"id_product"`
+    Name        string `db:"name" json:"name"`
+    Desc        string `db:"desc" json:"desc"`
+    Price       int    `db:"price" json:"price"`
+    ImagePath   string `db:"image_path" json:"image_path"`
+    TotalReview int    `db:"total_review" json:"total_review"`
+}
