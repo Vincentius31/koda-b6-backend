9000c748e2be14a8e5268cc3ad9b99888a3a3705
Vincentius <vincentius85@gmail.com>
2026-03-12 09:10:49 +0700

    feat: adding new DTO for get the latest reviews

--- internal/models/review.go
+++ internal/models/review.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type Review struct {
 	IDReview  int     `json:"id_review"`
 	UserID    int     `json:"user_id"`
@@ -20,4 +22,11 @@ type UpdateReviewRequest struct {
 	ProductID *int     `json:"product_id"`
 	Messages  *string  `json:"messages"`
 	Rating    *float64 `json:"rating"`
+}
+
+type ReviewLanding struct {
+	Fullname  string    `db:"fullname" json:"fullname"`
+	Messages  string    `db:"messages" json:"messages"`
+	Rating    float64   `db:"rating" json:"rating"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
