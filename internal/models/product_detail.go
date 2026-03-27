ea70af15d68c3d946af460759f0f08d029dc2b9b
Vincentius <vincentius85@gmail.com>
2026-03-27 23:34:26 +0700

    feat: adding new model for the size and variant

--- internal/models/product_detail.go
+++ internal/models/product_detail.go
@@ -1,17 +1,27 @@
 package models
 
+type DetailSize struct {
+	SizeName        string `json:"size_name"`
+	AdditionalPrice int    `json:"additional_price"`
+}
+
+type DetailVariant struct {
+	VariantName     string `json:"variant_name"`
+	AdditionalPrice int    `json:"additional_price"`
+}
+
 type ProductDetail struct {
-	IDProduct     int      `json:"id_product"`
-	Name          string   `json:"name"`
-	Desc          string   `json:"desc"`
-	Price         int      `json:"price"`
-	DiscountRate  float64  `json:"discount_rate"`
-	DiscountPrice int      `json:"discount_price"`
-	Rating        float64  `json:"rating"`
-	TotalReview   int      `json:"total_review"`
-	Images        []string `json:"images"`
-	Sizes         []string `json:"sizes"`
-	Variants      []string `json:"variants"`
+	IDProduct     int             `json:"id_product"`
+	Name          string          `json:"name"`
+	Desc          string          `json:"desc"`
+	Price         int             `json:"price"`
+	DiscountRate  float64         `json:"discount_rate"`
+	DiscountPrice int             `json:"discount_price"`
+	Rating        float64         `json:"rating"`
+	TotalReview   int             `json:"total_review"`
+	Images        []string        `json:"images"`
+	Sizes         []DetailSize    `json:"sizes"`
+	Variants      []DetailVariant `json:"variants"`
 }
 
 type ProductDetailResponse struct {
