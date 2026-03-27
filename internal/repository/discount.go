2fe00aad95cb35d4d7c09b33952d0bbd32194056
Vincentius <vincentius85@gmail.com>
2026-03-27 15:43:17 +0700

    fix: bring back the logic and still duplicate for the promos

--- internal/repository/discount.go
+++ internal/repository/discount.go
@@ -21,39 +21,66 @@ func (r *DiscountRepository) Create(ctx context.Context, d models.Discount) erro
 }
 
 func (r *DiscountRepository) FindAll(ctx context.Context) ([]models.Discount, error) {
-	query := `SELECT id AS id_discount, product_id, discount_rate, description, is_flash_sale FROM discount`
+	query := `SELECT id_discount, product_id, discount_rate, description, is_flash_sale FROM discount`
 	rows, err := r.db.Query(ctx, query)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	return pgx.CollectRows(rows, pgx.RowToStructByName[models.Discount])
+	var promos []models.Discount
+	for rows.Next() {
+		var d models.Discount
+
+		err := rows.Scan(
+			&d.IDDiscount,
+			&d.ProductID,
+			&d.DiscountRate,
+			&d.Description,
+			&d.IsFlashSale,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		promos = append(promos, d)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return promos, nil
 }
 
 func (r *DiscountRepository) FindByID(ctx context.Context, id int) (*models.Discount, error) {
-	query := `SELECT id AS id_discount, product_id, discount_rate, description, is_flash_sale FROM discount WHERE id = $1`
-	rows, err := r.db.Query(ctx, query, id)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+	query := `SELECT id_discount, product_id, discount_rate, description, is_flash_sale FROM discount WHERE id_discount = $1`
+	var d models.Discount
 
-	d, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.Discount])
+	err := r.db.QueryRow(ctx, query, id).Scan(
+		&d.IDDiscount,
+		&d.ProductID,
+		&d.DiscountRate,
+		&d.Description,
+		&d.IsFlashSale,
+	)
+
 	if err != nil {
 		return nil, err
 	}
+
 	return &d, nil
 }
 
 func (r *DiscountRepository) Update(ctx context.Context, id int, d models.Discount) error {
-	query := `UPDATE discount SET product_id=$1, discount_rate=$2, description=$3, is_flash_sale=$4 WHERE id=$5`
+	query := `UPDATE discount SET product_id=$1, discount_rate=$2, description=$3, is_flash_sale=$4 WHERE id_discount=$5`
 	_, err := r.db.Exec(ctx, query, d.ProductID, d.DiscountRate, d.Description, d.IsFlashSale, id)
 	return err
 }
 
 func (r *DiscountRepository) Delete(ctx context.Context, id int) error {
-	query := `DELETE FROM discount WHERE id = $1`
+	query := `DELETE FROM discount WHERE id_discount = $1`
 	_, err := r.db.Exec(ctx, query, id)
 	return err
 }
