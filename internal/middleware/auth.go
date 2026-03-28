ec845aa5086caf43ca12b8fad1a18dfea89fc746
Vincentius <vincentius85@gmail.com>
2026-03-29 04:07:38 +0700

    fix: fixing bugs for the jwt

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"koda-b6-backend/internal/models"
 	"net/http"
 	"os"
@@ -14,13 +15,21 @@ func AuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		authHeader := ctx.GetHeader("Authorization")
 		if authHeader == "" {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.WebResponse{Success: false, Message: "Authorization header is required"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.WebResponse{
+				Success: false,
+				Message: "Authorization header is required",
+				Data:    nil,
+			})
 			return
 		}
 
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.WebResponse{Success: false, Message: "Invalid format"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.WebResponse{
+				Success: false,
+				Message: "Invalid authorization format. Please use 'Bearer <token>'",
+				Data:    nil,
+			})
 			return
 		}
 
@@ -28,23 +37,36 @@ func AuthMiddleware() gin.HandlerFunc {
 		secret := os.Getenv("APP_SECRET")
 
 		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
 			return []byte(secret), nil
 		})
 
+		// Jika token expired atau signature salah
 		if err != nil || !token.Valid {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.WebResponse{Success: false, Message: "Sesi habis, silakan login ulang"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.WebResponse{
+				Success: false,
+				Message: "Invalid or expired token",
+				Data:    nil,
+			})
 			return
 		}
 
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if ok && token.Valid {
-			if val, exists := claims["user_id"]; exists {
-				if floatVal, ok := val.(float64); ok {
-					ctx.Set("user_id", int(floatVal))
-				}
+			if userIDFloat, exists := claims["user_id"].(float64); exists {
+				ctx.Set("user_id", int(userIDFloat))
 			}
+			
 			ctx.Set("email", claims["email"])
-			ctx.Next()
+			ctx.Next() 
+		} else {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.WebResponse{
+				Success: false,
+				Message: "Unauthorized. Please login.",
+				Data:    nil,
+			})
 		}
 	}
 }
